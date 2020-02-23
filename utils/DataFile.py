1313b26040c0e9c3d80055cac94169c233c666f6
Sarah Hultin <smhultin@rams.colostate.edu>
2020-02-23 00:24:58 -0800

    added search functions

--- utils/DataFile.py
+++ utils/DataFile.py
@@ -2,12 +2,26 @@ import pandas as pd
 
 data_filepath = '../data.csv'
 
-def load() :
+def loadIn() :
     global data_filepath
     global df
     df = pd.read_csv(data_filepath)
     return df
+
+def loadOut(df) :
+    global data_filepath
+    #df.write_csv(data_filepath)
+    
+def addNewMember(df, first, last, email, gradDate, big, little, inProgram, seekingLittle, seekingBig, mentorLead, notes) :
+    df = df.append({'First': first, 'Last': last, 'Email': email}, ignore_index=True)
+    return df
+    
+def searchFirstName(df, first) :
+    return df.query('First == @first')
     
 
 if __name__=='__main__' :
-    load()
+    df = loadIn()
+    df = addNewMember(df, 'Westin','Musser','wstnmssr@gmail.com','S20','','',True,False,False,'','')
+    print(searchFirstName(df, 'Sarah'))
+    
